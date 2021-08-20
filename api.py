b2999d0ba4d1352b31120341d54aad003fd9d199
Nya Candy <dev@candinya.com>
2021-08-20 16:49:20 +0800

    chore: print auth key

--- api.py
+++ api.py
@@ -4,17 +4,23 @@ import os
 
 from core import translate
 
+env_auth_key = ''
+
+try:
+    env_auth_key = os.environ['AUTH_KEY']
+    print('AUTH_KEY: ' + env_auth_key)
+except KeyError:
+    print('No AUTH_KEY specified.')
+
 app = Flask('NyaMT')
 
 
 @app.route('/', methods=['POST'])
 def post_translation():
     auth_key = request.args.get('auth_key')
-    try:
-        if auth_key != os.environ['AUTH_KEY']:
-            abort(401)
-    except KeyError:
-        print('No AUTH_KEY specified.')
+
+    if env_auth_key != '' and auth_key != env_auth_key:
+        abort(401)
 
     text = request.args.get('text')
     target_lang = request.args.get('target_lang').lower()
